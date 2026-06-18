a5275e870d19aaa15e5b957dd49b85bfc998a66a
Prashraj78 <105579517+Prashraj78@users.noreply.github.com>
2026-06-18 11:37:50 +0530

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={images.profile} alt="profile_bg"  style={{borderRadius: "20%"}}/>
+
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
